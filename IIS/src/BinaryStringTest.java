90ec3a037a8ff84a363cc66397b38966437f35d2
agonzalez <ertonio84@gmail.com>
2015-05-29 21:15:33 +0200

    Ejercicio 1

--- IIS/src/BinaryStringTest.java
+++ IIS/src/BinaryStringTest.java
@@ -6,8 +6,12 @@ import org.junit.Test;
 public class BinaryStringTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testLengt() {
+		BinaryString bitsetA = new BinaryString(10);
+		BinaryString bitsetB = new BinaryString("0110010010");
+		
+		assertEquals(10,bitsetA.length());
+		assertEquals(10,bitsetB.length());
 	}
 
 }
